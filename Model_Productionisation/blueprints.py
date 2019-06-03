7937b5422cad22e5fe932a02ac3accc2397c36ef
Harpal Sahota <harpal28sahota@gmail.com>
2019-06-03 21:49:59 +0100

    added blueprint logging

--- Model_Productionisation/blueprints.py
+++ Model_Productionisation/blueprints.py
@@ -1,12 +1,40 @@
 """
 Blueprints for the models
 """
+import logging
 
-from flask import Blueprint
+from flask import (
+    request,
+    Blueprint
+)
 
 from views.property_price_prediction import PropertyPricePrediction
 
-price_prediction_blueprint = Blueprint('price_prediction_blueprint', __name__)
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+class BaseBlueprint(Blueprint):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.before_request(self._log_before_request)
+        self.after_request(self._log_post_request)
+
+    def _log_before_request(self, *args, **kwargs):
+        print(args, kwargs)
+        logger.info(f'{request.url_rule}')
+
+    def _log_post_request(self, *args, **kwargs):
+        print(args, kwargs)
+        return args[0]
+
+
+price_prediction_blueprint = BaseBlueprint('price_prediction_blueprint', __name__)
 
 price_prediction_blueprint.add_url_rule(
     '/property-price-prediction',
