f9f7ad68c4f42312c4e85893d6e9f1e62cbc64a6
Harpal Sahota <harpal28sahota@gmail.com>
2019-06-04 22:40:51 +0100

    added schema

--- Model_Productionisation/views/property_price_prediction.py
+++ Model_Productionisation/views/property_price_prediction.py
@@ -1,9 +1,14 @@
 
-from flask import jsonify
+from flask import (
+    request,
+    jsonify,
+)
 from flask.views import MethodView
 
 
 import joblib
+
+from common import schemas
 
 import settings
 
@@ -14,5 +19,8 @@ class PropertyPricePrediction(MethodView):
     methods = ['GET']
 
     def get(self):
+        features = request.args.to_dict()
+        price_prediction = schemas.PricePredictionFeaturesSchema().load(features)
+        print(price_prediction)
         predicted_price = MODEL.predict([[1, 0, 3]]).tolist()
         return jsonify({'predicted_price': predicted_price})
