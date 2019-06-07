24d1397baa3564a9d1ea2026535267c911bc83dc
Harpal Sahota <harpal.sahota@zpg.co.uk>
2019-06-07 17:18:23 +0100

    added loading of .env file and config.yml file

--- Model_Productionisation/settings.py
+++ Model_Productionisation/settings.py
@@ -1,1 +1,23 @@
+import os
+from os.path import (
+    dirname,
+    join,
+)
+from dotenv import load_dotenv
+import yaml
+
+# Load environment variables from the .env file
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 MODEL_NAME = 'rf_classifier.pkl'
+
+# Load custom settings from the YAML file specified by APP_SETTINGS_YAML
+custom_settings_filepath = os.environ.get('APP_SETTINGS_YAML')
+if custom_settings_filepath is not None:
+    with open(custom_settings_filepath) as infile:
+        globals().update(yaml.safe_load(infile))
+
+for var, value in os.environ.items():
+    if var in globals():
+        globals()[var] = value
