fa5be69b323215a1cfa7b30d23930044d8fc9b71
Harpal Sahota <harpal.sahota@zpg.co.uk>
2019-06-07 17:34:35 +0100

    added validation template and post load to a data class

--- Model_Productionisation/common/schemas.py
+++ Model_Productionisation/common/schemas.py
@@ -1,10 +1,34 @@
 from marshmallow import (
-    Schema,
     fields,
+    post_load,
+    validates,
+    Schema,
+    ValidationError
 )
+
+from common.data_models import PricePredictionFeaturesModel
+
 
 class PricePredictionFeaturesSchema(Schema):
     is_house = fields.Int()
     has_garden = fields.Int()
     n_bedrooms = fields.Int()
 
+    @validates('is_house')
+    def validate_is_house(self, value):
+        pass
+
+    @validates('has_garden')
+    def validate_is_house(self, value):
+        pass
+
+    @validates('n_bedrooms')
+    def validate_is_house(self, value):
+        pass
+
+    @post_load
+    def make_price_prediction_data_model(self, data):
+        """
+        Once the schema checks have passes create an instance of the model
+        """
+        return PricePredictionFeaturesModel(**data)
