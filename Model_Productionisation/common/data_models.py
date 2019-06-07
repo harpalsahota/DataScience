d9ca2ed91a656c339b64a532cd3612feb3040775
Harpal Sahota <harpal.sahota@zpg.co.uk>
2019-06-07 17:34:08 +0100

    added price prediction data model

---
+++ Model_Productionisation/common/data_models.py
@@ -0,0 +1,17 @@
+
+from dataclasses import dataclass
+
+
+@dataclass(init=True)
+class PricePredictionFeaturesModel:
+    """ Data Class for Price Prediction """
+
+    is_house: int
+    has_garden: int
+    n_bedrooms: int
+
+    def __repr__(self):
+        return (
+            f'<PricePredictionModel is_house={self.is_house} '
+            f'has_garden={self.has_garden} n_bedrooms={self.n_bedrooms}>'
+        )
