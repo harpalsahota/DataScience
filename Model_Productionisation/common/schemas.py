708a1e6ac0d8a3a48f450629bb2a39bede1c7a4a
Harpal Sahota <harpal28sahota@gmail.com>
2019-06-10 21:45:06 +0100

    addded schema checkes

--- Model_Productionisation/common/schemas.py
+++ Model_Productionisation/common/schemas.py
@@ -10,21 +10,24 @@ from common.data_models import PricePredictionFeaturesModel
 
 
 class PricePredictionFeaturesSchema(Schema):
-    is_house = fields.Int()
-    has_garden = fields.Int()
-    n_bedrooms = fields.Int()
+    is_house = fields.Integer()
+    has_garden = fields.Integer()
+    n_bedrooms = fields.Integer()
 
     @validates('is_house')
     def validate_is_house(self, value):
-        pass
+        if 0 < value > 1:
+            raise ValidationError('is_house must be either 0 or 1')
 
     @validates('has_garden')
-    def validate_is_house(self, value):
-        pass
+    def validate_has_garden(self, value):
+        if 0 < value > 1:
+            raise ValidationError('has_garden must be either 0 or 1')
 
     @validates('n_bedrooms')
-    def validate_is_house(self, value):
-        pass
+    def validate_n_bedrooms(self, value):
+        if value < 0:
+            raise ValidationError('n_bedrooms must be >= 0')
 
     @post_load
     def make_price_prediction_data_model(self, data):
