d835da40334e968eec9e7a7bda713d6c59f4520c
Harpal Sahota <harpal.sahota@zpg.co.uk>
2019-07-18 19:30:40 +0100

    cleaned script

--- Model_API_Example/wsgi.py
+++ Model_API_Example/wsgi.py
@@ -1,10 +1,1 @@
 from api import api
-
-import settings
-
-if __name__ == '__main__':
-    api.run(
-        host=settings.FLASK_IP,
-        port=int(settings.FLASK_PORT),
-        debug=False
-    )
