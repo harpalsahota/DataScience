457ee80a24512fa4f2b5166285bc898508c6c279
Harpal Sahota <harpal28sahota@gmail.com>
2020-01-16 21:49:44 +0000

    added doc strings

--- Recommendation_Systems/steam_games/download.py
+++ Recommendation_Systems/steam_games/download.py
@@ -1,7 +1,10 @@
 """Module to help with downloading data from steamspy API"""
 
 import json
-from typing import Dict
+from typing import (
+    Dict,
+    Union,
+)
 from urllib.request import urlopen
 
 
@@ -20,10 +23,25 @@ def download_all_json_data() -> Dict:
 
 def get_games_with_tag(tag: str) -> Dict:
     """
+    Obtain games that have a given tag
 
-    :param tag:
-    :return:
+    :type tag: str
+    :param tag: Tag to download data for
+    :rtype: Dict
+    :return: JSON object with the data
     """
     tag = tag.replace(' ', '+')
     response = urlopen(f'{STEAMSPY_URL}?request=tag&tag={tag}')
+    return json.load(response)
+
+def get_game_data(game_id: Union[int, str]) -> Dict:
+    """
+    Obtain the data for a given game including tag data
+
+    :type game_id: Union[str, int]
+    :param game_id: ID of the game
+    :rtype: Dict
+    :return: JSON object with the data
+    """
+    response = urlopen(f'{STEAMSPY_URL}?request=appdetails&appid={game_id}')
     return json.load(response)
