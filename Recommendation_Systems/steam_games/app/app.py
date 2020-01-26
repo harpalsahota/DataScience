ed12142691e6ee8fd8c53082a68562e6525b84d1
Harpal Sahota <harpal28sahota@gmail.com>
2020-01-26 22:21:55 +0000

    added post method for game recommendations

--- Recommendation_Systems/steam_games/app/app.py
+++ Recommendation_Systems/steam_games/app/app.py
@@ -2,6 +2,7 @@ from flask import (
     Flask,
     jsonify,
     render_template,
+    request,
 )
 from flask_material import Material
 
@@ -12,9 +13,16 @@ Material(app)
 
 @app.route('/')
 def index():
-    games2=['game1', 'game2', 'game3']
     games = {'BattleField': None, 'Age of Empires': None, 'game3': None}
-    return render_template('index.html', games=games, games2=games2)
+    return render_template('index.html', games=games)
+
+
+@app.route('/recommendation', methods=['POST'])
+def selected_game():
+    if request.method == 'POST':
+        data = request.json
+        print(data)
+        return jsonify(['game x', 'game y', 'game x'])
 
 
 if __name__ == '__main__':
