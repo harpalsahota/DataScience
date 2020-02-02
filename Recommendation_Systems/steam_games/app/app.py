afcc24e610a005ca2969efbaa39a8acb52f5ce09
Harpal Sahota <harpal28sahota@gmail.com>
2020-02-02 18:15:42 +0000

    added function to find closest games

--- Recommendation_Systems/steam_games/app/app.py
+++ Recommendation_Systems/steam_games/app/app.py
@@ -14,8 +14,13 @@ with open('./data/games_none.json', 'r') as in_json:
     GAME_AUTOCOMPLETE = json.load(in_json)
 with open('./data/games_with_tags_double_filter.json', 'r') as in_json:
     GAME_TAGS = json.load(in_json)
-GAME_WEIGHTS = np.load('./data/game_weights_200.pkl')
-TAG_WEIGHTS = np.load('./data/tag_weights_200.pkl')
+GAME_INDEX = {game: idx for idx, game in enumerate(GAME_TAGS)}
+INDEX_GAME = {idx: game for game, idx in GAME_INDEX.items()}
+
+GAME_WEIGHTS = np.load('./data/game_weights_200.pkl', allow_pickle=True)
+TAG_WEIGHTS = np.load('./data/tag_weights_200.pkl', allow_pickle=True)
+
+GAME_WEIGHTS = GAME_WEIGHTS / np.linalg.norm(GAME_WEIGHTS, axis=1).reshape((-1, 1))
 
 app = Flask(__name__)
 Material(app)
@@ -30,7 +35,16 @@ def index():
 def selected_game():
     if request.method == 'POST':
         data = request.json
-        return jsonify(GAME_TAGS[data['game']])
+        related = find_closest(GAME_WEIGHTS[GAME_INDEX[data['game']]])
+        tags = GAME_TAGS[data['game']]
+        return jsonify({'related': related, 'tags': tags})
+
+
+def find_closest(game_embedding: np.array):
+    dists = np.dot(GAME_WEIGHTS, game_embedding)
+    sorted_dists = np.argsort(dists)
+    closest = sorted_dists[-6:-1]
+    return [(INDEX_GAME[i], f'{dists[i]:.{2}}') for i in reversed(closest)]
 
 
 if __name__ == '__main__':
