443086993e574cbad4d673ffad33b77c6f4ee839
Harpal Sahota <harpal28sahota@gmail.com>
2020-02-11 20:16:03 +0000

    added doc strings

--- Recommendation_Systems/steam_games/app/app.py
+++ Recommendation_Systems/steam_games/app/app.py
@@ -1,5 +1,10 @@
 import json
-from typing import List
+from typing import (
+    List,
+    Mapping,
+    Tuple,
+    Union,
+)
 
 from flask import (
     Flask,
@@ -10,6 +15,7 @@ from flask import (
 from flask_material import Material
 import numpy as np
 
+JSON = Union[str, int, float, bool, None, Mapping[str, 'JSON'], List['JSON']]
 
 with open('./data/games_none.json', 'r') as none_json,\
     open('./data/games_with_tags_double_filter.json', 'r') as data_json:
@@ -39,7 +45,10 @@ Material(app)
 
 
 @app.route('/')
-def index():
+def index() -> render_template:
+    """
+    App route method
+    """
     return render_template(
         'index.html',
         games_autocomplete=GAME_AUTOCOMPLETE,
@@ -50,62 +59,84 @@ def index():
 
 
 @app.route('/recommendation', methods=['POST'])
-def selected_game():
+def selected_game() -> JSON:
+    """
+    For a selected game return the recommendations
+
+    :rtype: JSON
+    :return: JSON containing the recommendations, tags and game embedding
+    """
     if request.method == 'POST':
         data = request.json
         embedding = GAME_WEIGHTS[GAME_INDEX[data['game']]]
         related = find_closest(embedding)
         tags = GAME_TAGS[data['game']]
         return jsonify({
-            'related': remove_self(data['game'], related),
+            'related': _remove_self(data['game'], related),
             'tags': tags,
             'embedding': embedding.tolist()
         })
 
 
 @app.route('/modify-embedding', methods=['POST'])
-def modify_embedding():
+def modify_embedding() -> JSON:
+    """
+    Modify the embedding of the selected game
+
+    :rtype: JSON
+    :return: JSON containing the new recommended games and modified embedding
+    """
     if request.method == 'POST':
         data = request.json
         if data['modification'] == 'addition':
-            related, embedding = add_tag(data['tag'], data['game'], data['embedding'])
+            related, embedding = add_tag(data['tag'], data['embedding'])
             return jsonify({
-                'related': remove_self(data['game'], related),
+                'related': _remove_self(data['game'], related),
                 'embedding': embedding.tolist()
             })
         else:
-            related, embedding = subtract_tag(data['tag'], data['game'], data['embedding'])
+            related, embedding = subtract_tag(data['tag'], data['embedding'])
             return jsonify({
-                'related': remove_self(data['game'], related),
+                'related': _remove_self(data['game'], related),
                 'embedding': embedding.tolist()
             })
 
 
-def remove_self(game, related):
+def _remove_self(game: str, related: List[Tuple[str, float]]) -> List[Tuple[str, float]]:
     """
+    Removes the selected game from the list of recommendations
 
-    :param game:
-    :param related:
-    :return:
+    :type game: str
+    :param game: Selected game
+    :type related: List[Tuple[str, float]]
+    :param related: Related games with similarity scores
+    :rtype: List[Tuple[str, float]]
+    :return: New recommendations with the selected game removed
     """
     return [i for i in related if i[0] != game][:10]
 
 
-def find_closest(game_embedding: np.array):
+def find_closest(game_embedding: np.array) -> List[Tuple[str, float]]:
+    """
+    Find the nearest games to the embedding based on cosine distance
+
+    :type game_embedding: np.array
+    :param game_embedding: Embedding of the selected game
+    :rtype: List[Tuple[str, float]]
+    :return: List of nearest game titles along with similarity score
+    """
     dists = np.dot(GAME_WEIGHTS, game_embedding)
     sorted_dists = np.argsort(dists)
     closest = sorted_dists[-11:]
     return [(INDEX_GAME[i], f'{dists[i]:.{2}}') for i in reversed(closest)]
 
 
-def add_tag(tag: str, game: str, embedding: List[float]) -> np.array:
+def add_tag(tag: str, embedding: List[float]) -> np.array:
     """
     Subtracts a tag embedding from a game embedding and normalises
 
     :type tag: str
     :param tag: Tag to add to game embedding
-    :type game: str
-    :param game: Game which tag embedding is added to
     :rtype: np.array
     :return: New game array with the tag embedding added
     """
@@ -116,14 +147,12 @@ def add_tag(tag: str, game: str, embedding: List[float]) -> np.array:
     )
 
 
-def subtract_tag(tag: str, game: str, embedding: List[float]) -> np.array:
+def subtract_tag(tag: str, embedding: List[float]) -> np.array:
     """
     Subtracts a tag embedding from a game embedding and normalises
 
     :type tag: str
     :param tag: Tag to subtract from game embedding
-    :type game: str
-    :param game: Game which tag embedding is subtracted from
     :rtype: np.array
     :return: New game array with the tag embedding removed
     """
